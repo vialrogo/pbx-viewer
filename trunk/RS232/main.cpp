1a3cdcd1e2aafabd88c2481ee9ba1c2a0e31ab1e
varg04444 <varg04444@58e77f3c-66d3-2f76-d9f4-08142b3b8c59>
2010-05-28 18:22:11 +0000

    RS232 added

---
+++ trunk/RS232/main.cpp
@@ -0,0 +1,19 @@
+/*
+ * File:   main.cpp
+ * Author: vialrogo
+ *
+ * Created on 28 de Maio de 2010, 13:11
+ */
+
+#include <QtGui/QApplication>
+
+int main(int argc, char *argv[]) {
+    // initialize resources, if needed
+    // Q_INIT_RESOURCE(resfile);
+
+    QApplication app(argc, argv);
+
+    // create and show your widgets here
+
+    return app.exec();
+}
