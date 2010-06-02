4d58a2cc9154426e4c9cea421e8ac21d1cdec98b
varg04444 <varg04444@58e77f3c-66d3-2f76-d9f4-08142b3b8c59>
2010-06-02 23:07:17 +0000

    Carpeta documentacion agregada

--- SQL/pbxviewer.sql
+++ SQL/pbxviewer.sql
@@ -3,8 +3,8 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tiempo de generación: 28-05-2010 a las 12:22:01
--- Versión del servidor: 5.1.46
+-- Tiempo de generación: 02-06-2010 a las 17:44:44
+-- Versión del servidor: 5.1.47
 -- Versión de PHP: 5.3.2
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
@@ -27,17 +27,45 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
 CREATE TABLE IF NOT EXISTS `llamada` (
   `lla_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `lla_hora` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `lla_hora` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `lla_origen` varchar(10) NOT NULL,
   `lla_destino` varchar(15) NOT NULL,
   `lla_codigocuenta` varchar(5) NOT NULL,
-  `lla_tipo` varchar(2) NOT NULL,
+  `lla_idTipo` bigint(20) unsigned NOT NULL,
   `lla_duracion` int(4) NOT NULL,
   `lla_costo` double NOT NULL,
-  UNIQUE KEY `lla_id` (`lla_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+  PRIMARY KEY (`lla_id`),
+  KEY `lla_idTipo` (`lla_idTipo`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=9 ;
 
 --
 -- Volcar la base de datos para la tabla `llamada`
 --
 
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tipo_llamada`
+--
+
+CREATE TABLE IF NOT EXISTS `tipo_llamada` (
+  `tip_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `tip_tarifa` int(4) NOT NULL,
+  PRIMARY KEY (`tip_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+--
+-- Volcar la base de datos para la tabla `tipo_llamada`
+--
+
+
+--
+-- Filtros para las tablas descargadas (dump)
+--
+
+--
+-- Filtros para la tabla `llamada`
+--
+ALTER TABLE `llamada`
+  ADD CONSTRAINT `llamada_ibfk_1` FOREIGN KEY (`lla_idTipo`) REFERENCES `tipo_llamada` (`tip_id`) ON DELETE NO ACTION ON UPDATE CASCADE;
