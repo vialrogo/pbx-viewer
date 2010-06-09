50e7e0b48404942e897dd4689aaefbbc6e337cd8
aritoma <aritoma@58e77f3c-66d3-2f76-d9f4-08142b3b8c59>
2010-06-09 00:11:35 +0000

    Añadidos archivos de traduccion

--- trunk/Desktop/moc_GUIDesktop.cpp
+++ trunk/Desktop/moc_GUIDesktop.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'GUIDesktop.h'
 **
-** Created: Mon Jun 7 13:23:11 2010
+** Created: Tue Jun 8 19:06:47 2010
 **      by: The Qt Meta Object Compiler version 62 (Qt 4.6.2)
 **
 ** WARNING! All changes made in this file will be lost!
