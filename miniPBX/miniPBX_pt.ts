9452f09320ee166ccf263105f424d4cdd52b142e
varg04444 <varg04444@58e77f3c-66d3-2f76-d9f4-08142b3b8c59>
2010-08-24 02:46:30 +0000

    Resolviendo problema de sincronizacion

---
+++ miniPBX/miniPBX_pt.ts
@@ -0,0 +1,52 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="pt_PT">
+<context>
+    <name>GUIMiniPBX</name>
+    <message>
+        <location filename="GUIMiniPBX.cpp" line="117"/>
+        <source>Error probando Conexion RS232</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="GUIMiniPBX.cpp" line="123"/>
+        <source>Error creando Conexion RS232</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="GUIMiniPBX.cpp" line="134"/>
+        <source>MiniPBX corriendo</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="GUIMiniPBX.cpp" line="145"/>
+        <source>Se ha detenido exitosamente</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="GUIMiniPBX.cpp" line="150"/>
+        <source>Idioma cambiado</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="GUIMiniPBX.cpp" line="175"/>
+        <source>&lt;h3&gt;PBX Viewer: MiniPBX RS232 a Socket 1.0&lt;/h3&gt;&lt;br&gt;Un MiniPBX de RS232 a Socket escrito en Qt.&lt;br&gt;&lt;br&gt;GNU Lesser General Public License&lt;br&gt;&lt;a href=&quot;http://www.gnu.org/licenses/lgpl.html&quot;&gt;http://www.gnu.org/licenses/lgpl.html&lt;/a&gt;&lt;br&gt;&lt;br&gt;Creado por :&lt;br&gt;Victor Alberto Romero Gonzalez&lt;br&gt;&lt;br&gt;Escuela de IngenierÃ­a ElÃ©ctrica Y ElectrÃ³nica&lt;br&gt;Universidad del Valle&lt;br&gt;2010</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="GUIMiniPBX.cpp" line="176"/>
+        <source>Acerca de</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="GUIMiniPBX.cpp" line="184"/>
+        <source>&lt;h3&gt;Bienvenido a la ayuda de PBX Viewer: MiniPBX RS232 a Socket&lt;/h3&gt;&lt;h4&gt;Ayuda&lt;/h4&gt;&lt;p&gt;Esta aplicaciÃ³n permite recibir tramas a travÃ©s del puerto RS232 y retransmitirlas a un puerto de red.La aplicaciÃ³n consta de 2 secciones,RS232 y Socket,las cuales contienen las configuraciones de cada una respectivamente.&lt;/p&gt;&lt;p&gt;Si presenta problemas de conexion con el puerto y/o socket,por favor consulte al administrador del sistema.&lt;/p&gt;&lt;h4&gt;Contribuye al proyecto&lt;/h4&gt;&lt;p&gt;El proyecto se encuentra bajo licencia LGPL. Para saber como contribuir a este proyecto, por favor contacte al autor.&lt;/p&gt;&lt;h4&gt;Contacto&lt;/h4&gt;&lt;p&gt;Victor Alberto Romero Gonzalez&lt;br&gt;varg04444@gmail.com&lt;/p&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="GUIMiniPBX.cpp" line="185"/>
+        <source>Ayuda</source>
+        <translation>Ajuda</translation>
+    </message>
+</context>
+</TS>
